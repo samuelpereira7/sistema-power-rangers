a76d9da28ee8e7d32b116eba8e9ab3bb5e2b34da
samuelpereira7 <samuelpereira@gec.inatel.br>
2017-10-12 13:55:27 -0300

    Atualizacao de nome do projeto e dos pacotes.

---
+++ sistema-megazords/src/java/br/inatel/ac8/power_rangers/util/FabricaConexao.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.inatel.ac8.power_rangers.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author samuel
+ */
+public class FabricaConexao {
+    
+    private static Connection conexao;
+    private static final String URL_CONEXAO = "jdbc:mysql://Localhost:3306/sistema-powerrangers";
+    private static final String USUARIO = "root";
+    private static final String SENHA = "root";
+
+    public static Connection getConexao() {
+        if(conexao == null)
+        {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                conexao = DriverManager.getConnection(URL_CONEXAO, USUARIO, SENHA);
+            } catch (SQLException ex) {
+                Logger.getLogger(FabricaConexao.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(FabricaConexao.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        return conexao;
+    }
+    
+    public static void fecharConexao() {
+        if(conexao != null)
+        {
+            try {
+                conexao.close();
+                conexao = null;
+            } catch (SQLException ex) {
+                Logger.getLogger(FabricaConexao.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    
+    
+}
